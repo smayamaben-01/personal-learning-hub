581879ea57a43df36a26b51745953691b340691a
Smaya Maben <smayamaben1@gmail.com>
2026-07-30 12:21:39 +0530

    feat: add SSL support for Aiven MySQL connection

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
-
+DB_SSL_CA = os.getenv('DB_SSL_CA')
 SECRET_KEY = os.getenv('SECRET_KEY')
--- db.py
+++ db.py
@@ -4,7 +4,10 @@ import config
 def get_db_connection():
     return mysql.connector.connect(
         host=config.DB_HOST,
+        port=config.DB_PORT,
         user=config.DB_USER,
         password=config.DB_PASSWORD,
-        database=config.DB_NAME
+        database=config.DB_NAME,
+        ssl_ca=config.DB_SSL_CA,
+        ssl_verify_cert=True
     )
